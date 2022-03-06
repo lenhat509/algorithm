be84f5c10f4d362a829f1479488ff524554a63bc
Nhat Le <nhat.n.le@sjsu.edu>
2022-03-05 22:12:41 -0800

    commit

--- radixSort.js
+++ radixSort.js
@@ -1,36 +1,74 @@
 function radixSort(array) {
-    let max = Math.max(...array);
+    const max = Math.max(...array);
     let power = 0;
+    let buckets = new Array(10);
+    for (let i = 0; i < buckets.length; i++) buckets[i] = new Queue();
     while (Math.floor(max / 10 ** power) > 0) {
-        sort(array, power);
+        distribute(buckets, power, array);
+        collect(buckets, array);
         power++;
     }
+
     return array;
 }
 
-function sort(array, power) {
-    let counting = new Array(10).fill(0);
-    let sortedArray = new Array(array.length);
+function distribute(buckets, power, array) {
     for (let i = 0; i < array.length; i++) {
-        const remainder = Math.floor(array[i] / 10 ** power);
-        const digit = remainder % 10;
-        counting[digit]++;
+        let remainder = Math.floor(array[i] / 10 ** power);
+        let digit = remainder % 10;
+        buckets[digit].enqueue(array[i]);
     }
+}
 
-    for (let i = 1; i < counting.length; i++) {
-        counting[i] += counting[i - 1];
+function collect(buckets, array) {
+    let index = 0;
+    for (let i = 0; i < buckets.length; i++) {
+        while (buckets[i].size > 0) {
+            array[index++] = buckets[i].dequeue();
+        }
     }
-    for (let i = array.length - 1; i >= 0; i--) {
-        const remainder = Math.floor(array[i] / 10 ** power);
-        const digit = remainder % 10;
-        const position = counting[digit];
-        sortedArray[position - 1] = array[i];
-        counting[digit]--;
+}
+class Queue {
+    head = null;
+    tail = null;
+    size = 0;
+    constructor() {}
+    enqueue(value) {
+        let node = new Node(value);
+        if (this.size == 0) {
+            this.head = node;
+            this.tail = node;
+            this.size++;
+        } else {
+            this.tail.next = node;
+            this.tail = this.tail.next;
+            this.size++;
+        }
     }
-    console.log(array);
-    console.log(sortedArray);
-    for (let i = 0; i < array.length; i++) {
-        array[i] = sortedArray[i];
+    dequeue() {
+        if (this.size > 0) {
+            if (this.head == this.tail) {
+                let temp = this.head;
+                this.head = null;
+                this.tail = null;
+                this.size--;
+                return temp.value;
+            } else {
+                let temp = this.head;
+                this.head = this.head.next;
+                temp.next = null;
+                this.size--;
+                return temp.value;
+            }
+        }
+        return null;
+    }
+}
+class Node {
+    value = null;
+    next = null;
+    constructor(value) {
+        this.value = value;
     }
 }
 // alternative
