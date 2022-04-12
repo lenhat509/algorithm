d9bbe54b7a38670ec876e7e9bbc84440d87d5798
Nhat Le <nhat.n.le@sjsu.edu>
2022-04-11 19:24:36 -0700

    commit

---
+++ nonconstructiblechange.js
@@ -0,0 +1,9 @@
+function nonConstructibleChange(coins) {
+    coins.sort((a, b) => a - b);
+    let change = 0;
+    for (let i = 0; i < coins.length; i++) {
+        if (coins[i] > change + 1) return change + 1;
+        change += coins[i];
+    }
+    return change + 1;
+}
