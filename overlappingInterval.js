41bf2903bd78cc397eaedc1fff930706795b02bd
Nhat Le <nhat.n.le@sjsu.edu>
2022-08-25 15:18:16 -0700

    algo merging overlapping intervals

---
+++ overlappingInterval.js
@@ -0,0 +1,19 @@
+function mergeOverlappingIntervals(array) {
+    const result = [];
+    array.sort((a, b) => a[0] - b[0]);
+    let i = 0;
+    let interval = array[0];
+    while (i < array.length) {
+        if (i !== array.length - 1 && interval[1] >= array[i + 1][0]) {
+            interval[1] = Math.max(array[i + 1][1], interval[1]);
+        } else {
+            result.push(interval);
+            if (i !== array.length - 1) interval = array[i + 1];
+        }
+        i++;
+    }
+    return result;
+}
+
+// Do not edit the line below.
+exports.mergeOverlappingIntervals = mergeOverlappingIntervals;
