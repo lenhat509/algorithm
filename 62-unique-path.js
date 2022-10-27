126f05741a6e906dc7a4f147cce337d35af0b5b6
Nhat Le <nhat.n.le@sjsu.edu>
2022-10-26 20:23:04 -0700

    unique path optimal leetcode

--- 62-unique-path.js
+++ 62-unique-path.js
@@ -1,3 +1,4 @@
+// 0(2^n) O(m + n)
 var uniquePaths = function (m, n) {
     return findPath(0, 0, m, n);
 };
@@ -9,4 +10,14 @@ const findPath = (r, c, m, n) => {
     let right = findPath(r, c + 1, m, n);
     let bottom = findPath(r + 1, c, m, n);
     return right + bottom;
+};
+
+// O(m * n) O(n)
+var uniquePaths = function (m, n) {
+    let row = new Array(n);
+    row.fill(1);
+
+    for (let i = m - 2; i >= 0; i--)
+        for (let j = n - 2; j >= 0; j--) row[j] = row[j] + row[j + 1];
+    return row[0];
 };
