b88aca7f2dfb1a828b1104b8d0dfce078a2b4a23
Nhat Le <nhat.n.le@sjsu.edu>
2022-10-27 20:26:40 -0700

    tree distance

--- file.cs
+++ file.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 
-
-
 class Result
 {
 
@@ -71,7 +69,7 @@ public class BST
         }
     }
 
-    public Tracking findDistance(BSTNode current, int nodeA, int nodeB)
+    public Tracking findDistance(BSTNode? current, int nodeA, int nodeB)
     {
         if (current == null)
             return new Tracking(0, -1);
@@ -91,9 +89,12 @@ public class BST
         if (leftDistance >= 0 && rightDistance >= 0)
             return new Tracking(2, leftDistance + rightDistance + 2);
 
-        int targetsNum = leftTracking.targetsFound + rightTracking.targetsFound;
-        int distance = Math.Max(leftTracking.distance, rightTracking.distance);
-        return new Tracking(targetsNum, distance);
+        int targetsN = leftTracking.targetsFound + rightTracking.targetsFound;
+        int distMax = Math.Max(leftTracking.distance, rightTracking.distance);
+        int dist = distMax;
+        if (targetsN != 2)
+            dist = distMax == -1 ? distMax : distMax + 1;
+        return new Tracking(targetsN, dist);
     }
 
 
@@ -118,34 +119,5 @@ public class BSTNode
         this.value = value;
         this.left = null;
         this.right = null;
-    }
-}
-
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        var textWriter = new System.IO.StreamWriter(System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        var inputString = System.Console.ReadLine();
-        inputString = inputString.Replace(",", "");
-        var vectorStartIndex = inputString.IndexOf('{');
-        var vectorEndIndex = inputString.IndexOf('}');
-        var vectorInputString = inputString.Substring(vectorStartIndex + 1, vectorEndIndex - vectorStartIndex - 1);
-
-        var values = new List<int>();
-
-        foreach (var value in vectorInputString.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries))
-            values.Add(System.Convert.ToInt32(value));
-
-        var nodesInputString = inputString.Substring(vectorEndIndex + 1, inputString.Length - vectorEndIndex - 1);
-        var nodesInput = nodesInputString.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
-        var nodeA = System.Convert.ToInt32(nodesInput[0]);
-        var nodeB = System.Convert.ToInt32(nodesInput[1]);
-
-        textWriter.Write(Result.BSTdistance(values, nodeA, nodeB));
-
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
